6348d6b70507fe198eb70ba4ae3187fecd1f687d
NikitaPonamarev <105862143+NikitaPonamarev@users.noreply.github.com>
2024-03-07 00:08:05 +0300

    feat: add interface

---
+++ DataBase.cpp
@@ -0,0 +1,14 @@
+class DataBaseModule {
+public:
+	void Connection() {
+		//Подключение к базе данных игроков
+	}
+
+	void Authorisation() {
+		//Авторизация игрока в клиенте
+	}
+
+	void PlayerDataModify() {
+		//Работа с даннами игроков
+	}
+};
